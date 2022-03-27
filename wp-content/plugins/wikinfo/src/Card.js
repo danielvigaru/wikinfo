bef47ba59832197539135c376c02dfc95c5d2351
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2022-03-27 19:42:15 +0300

    fix href stuff

--- wp-content/plugins/wikinfo/src/Card.js
+++ wp-content/plugins/wikinfo/src/Card.js
@@ -1,6 +1,6 @@
 export default function Card({ blockProps, image, link, summary, title }) {
     return (
-        <a {...blockProps} href={link} target="_blank" className="wikinfo">
+        <a {...blockProps} href={link} target="_blank" rel="noopener" className="wikinfo">
             <span id="title">{title}</span>
             {image && <img src={image} aria-labelledby="title" id="thumbnail" />}
             <div id="summary">{summary}</div>
